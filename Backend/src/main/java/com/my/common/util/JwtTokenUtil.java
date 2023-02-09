7ec684415a01132c74ba53fc4fe0f58dccf14cb2
jhno96 <jhno96@gmail.com>
2023-02-09 17:05:40 +0900

    Feat. Create Token and parse claims from refreshToken

--- Backend/src/main/java/com/my/common/util/JwtTokenUtil.java
+++ Backend/src/main/java/com/my/common/util/JwtTokenUtil.java
@@ -1,8 +1,11 @@
 package com.my.common.util;
 
+import com.my.common.exception.JwtInvalidException;
+import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
 import java.util.Date;
 
 @Component
@@ -26,6 +29,42 @@ public class JwtTokenUtil {
         this.refreshSecretKeyBytes = refreshSecretKey.getBytes();
         this.expireMin = expireMin;
         this.refreshExpireMin = refreshExpireMin;
+    }
+
+    private String createToken(String userid, String authority, byte[] secretKeyBytes, int expireMin) {
+        Date now = new Date();
+        Claims claims = Jwts.claims().setSubject(userid);
+        claims.put(KEY_ROLES, Collections.singleton(authority));
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + (long) ONE_MINUTE * expireMin))
+                .signWith(SignatureAlgorithm.HS256, secretKeyBytes).compact();
+    }
+
+    public String createAccessToken(String userName, String authority) {
+        return createToken(userName, authority, secretKeyBytes, expireMin);
+    }
+
+    public String createRefreshToken(String userName, String authority) {
+        return createToken(userName, authority, refreshSecretKeyBytes, refreshExpireMin);
+    }
+
+    public Claims parseClaimsFromRefreshToken(String jsonWebToken) {
+        Claims claims;
+        try {
+            claims = Jwts.parser().setSigningKey(refreshSecretKeyBytes).parseClaimsJws(jsonWebToken).getBody();
+        } catch (SignatureException signatureException) {
+            throw new JwtInvalidException("signature key is different", signatureException);
+        } catch (ExpiredJwtException expiredJwtException) {
+            throw new JwtInvalidException("expired token", expiredJwtException);
+        } catch (MalformedJwtException malformedJwtException) {
+            throw new JwtInvalidException("malformed token", malformedJwtException);
+        } catch (IllegalArgumentException illegalArgumentException) {
+            throw new JwtInvalidException("using illegal argument like null", illegalArgumentException);
+        }
+        return claims;
     }
 
 }
